482d9deb297cc5c73a9731bdb0001aeae1161565
vm2291 <vm2291@nyu.edu>
2024-11-01 11:55:35 -0400

    changes

---
+++ week06/week6Application/week6Application/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  week6Application
+//
+//  Created by Vlera Mehani on 01/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("wElCoMe, let's get that sleep right!")
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ week06/week6Application/week6Application/week6ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  week6ApplicationApp.swift
+//  week6Application
+//
+//  Created by Vlera Mehani on 01/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct week6ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
