63da1a01246138036d10797fa749ec8a478c485c
vm2291 <vm2291@nyu.edu>
2024-10-31 23:34:48 -0400

    Week05

---
+++ week05/sleepScheduler2/sleepScheduler/ContentView.swift
@@ -0,0 +1,75 @@
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("wakeUpTime") private var wakeUpTime = Date()
+    @State private var currentTime = Date()
+    @AppStorage("sleepDuration") private var sleepDuration: TimeInterval = 0
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Sleep Scheduler")
+                    .font(.largeTitle)
+                    .padding()
+
+                Text("Current Time: \(currentTime, formatter: DateFormatter.timeFormatter)")
+                    .font(.headline)
+                    .padding()
+
+                DatePicker("Wake Up Time", selection: $wakeUpTime, displayedComponents: .hourAndMinute)
+                    .datePickerStyle(WheelDatePickerStyle())
+                    .labelsHidden()
+                    .padding()
+
+                Text("Sleep Duration: \(formattedSleepDuration)")
+                    .font(.title)
+                    .padding()
+
+                NavigationLink(destination: CountdownView()) {
+                    Text("Go to Sleep Timer")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                .padding()
+            }
+            .onAppear(perform: updateTime)
+        }
+    }
+
+    var formattedSleepDuration: String {
+        let hours = Int(sleepDuration) / 3600
+        let minutes = Int(sleepDuration) % 3600 / 60
+        return String(format: "%d hours %02d minutes", hours, minutes)
+    }
+
+    func updateTime() {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.currentTime = Date()
+            self.calculateSleepDuration()
+        }
+    }
+
+    func calculateSleepDuration() {
+        let calendar = Calendar.current
+        var sleepTime = calendar.date(bySettingHour: calendar.component(.hour, from: wakeUpTime),
+                                      minute: calendar.component(.minute, from: wakeUpTime),
+                                      second: 0,
+                                      of: currentTime) ?? currentTime
+        
+        if sleepTime <= currentTime {
+            sleepTime = calendar.date(byAdding: .day, value: 1, to: sleepTime) ?? sleepTime
+        }
+        
+        sleepDuration = sleepTime.timeIntervalSince(currentTime)
+    }
+}
+
+extension DateFormatter {
+    static let timeFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .medium
+        return formatter
+    }()
+}
