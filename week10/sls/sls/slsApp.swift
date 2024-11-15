412ebb494a913191b0164f39bf22ef7e3ac0cba2
vm2291 <vm2291@nyu.edu>
2024-11-15 09:25:17 -0500

    .
    newproject

---
+++ week10/sls/sls/slsApp.swift
@@ -0,0 +1,17 @@
+//
+//  slsApp.swift
+//  sls
+//
+//  Created by Vlera Mehani on 15/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct slsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
