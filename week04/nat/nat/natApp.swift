f54464a41fa4375937f1121ac9eb24466d98f113
vm2291 <vm2291@nyu.edu>
2024-11-30 00:05:55 -0500

    .
    .

---
+++ week04/nat/nat/natApp.swift
@@ -0,0 +1,17 @@
+//
+//  natApp.swift
+//  nat
+//
+//  Created by Vlera Mehani
+//
+
+import SwiftUI
+
+@main
+struct natApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
